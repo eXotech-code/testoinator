ea1b03d4f2ba71482eb0c231d0820f8fcf2c03be
Jakub Piskiewicz <jakub@piskiewicz.org>
2024-05-27 09:15:18 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import { goto, afterNavigate } from '$app/navigation';
+
+	import questions from '$lib/questions.json';
+
+	type Question = (typeof questions)[0];
+
+	const randomInt = (max: number): number => Math.floor(Math.random() * max);
+
+	const shuffle = (current: Question): Question => {
+		let left = current.answers.map((answer, i) => ({
+			correct: current.correct.includes(i),
+			answer
+		}));
+		current.correct = [];
+		for (let i = 0; i < current.answers.length; i++) {
+			const choice = left.splice(randomInt(left.length - 1), 1)[0];
+			current.answers[i] = choice.answer;
+			if (choice.correct) current.correct.push(i);
+		}
+
+		return current;
+	};
+
+	const submitAnswer = () => {
+		if (current.correct.every((val) => selectedAnswers[val])) {
+			if (left.length === 0) {
+				goto('/win');
+				return;
+			}
+			selectedAnswers = Array(4).fill(false);
+			current = left.splice(randomInt(left.length - 1), 1)[0];
+			current = shuffle(current);
+		}
+	};
+
+	const select = (i: number) => {
+		selectedAnswers[i] = !selectedAnswers[i];
+		if (current.correct.length === 1) selectedAnswers = selectedAnswers.map((_, j) => i === j);
+	};
+
+	let left = questions;
+	let current = left.splice(randomInt(questions.length - 1), 1)[0];
+	let selectedAnswers: boolean[] = Array(4).fill(false);
+
+	afterNavigate(() => (left = questions));
+</script>
+
+<main>
+	<h2>{current.question}</h2>
+	<div>
+		{#each current.answers as answer, i}
+			<button
+				class={`selectable ${selectedAnswers[i] ? 'selected' : ''}`}
+				on:click={() => select(i)}
+			>
+				{answer}
+			</button>
+		{/each}
+		<button class="next" on:click={submitAnswer}>{left.length === 0 ? 'Koniec' : 'Dalej'}</button>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
+		width: 20rem;
+	}
+
+	button {
+		border: 1px solid #000;
+		border-radius: 4px;
+		padding: 0.25rem;
+		width: 100%;
+	}
+	.selectable {
+		background: #fff;
+	}
+
+	.selected,
+	.next {
+		background: #000;
+		color: #fff;
+	}
+</style>
---
+++ src/routes/win/+page.svelte
@@ -0,0 +1,22 @@
+<main>
+	<h1>You won you fucken piece of fish</h1>
+	<a href="/">Spróbuj jeszcze raz</a>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 5rem;
+		justify-content: center;
+		align-items: center;
+	}
+
+	a {
+		text-decoration: none;
+		background: #000;
+		color: #fff;
+		padding: 0.5rem 1rem;
+		border-radius: 4px;
+	}
+</style>
