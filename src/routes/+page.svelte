4294cc32475d8a01ad2e25a1467d7efa1fec71cc
Jakub Piskiewicz <jakub@piskiewicz.org>
2024-05-27 20:19:23 +0200

    Move footer to layout file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Footer from '$lib/components/Footer.svelte';
 	import { goto, afterNavigate } from '$app/navigation';
 
 	import questions from '$lib/questions.json';
@@ -75,19 +74,8 @@
 		<p>{counter}/{questions.length}</p>
 	</div>
 </main>
-<Footer />
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-
-	:global(body) {
-		height: 100%;
-		font-family: 'Roboto', sans-serif;
-		display: flex;
-		flex-direction: column;
-		gap: 10rem;
-	}
-
 	main {
 		display: flex;
 		flex-direction: column;
